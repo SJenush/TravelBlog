351d441c2e89ad5044334da48fb12e04b12ddd6d
SJenush <jenushsjiffin@gmail.com>
2024-10-30 15:27:34 +0530

    Website to PWA

---
+++ service-worker.js
@@ -0,0 +1,61 @@
+const CACHE_NAME = 'travelblog-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/viewBlog.html',
+    '/createBlog.html',
+    '/manifest.json',
+    '/index.css',
+    '/viewBlog.css',
+    '/createBlog.css',
+    '/signup.html',
+    'img/index.jpg',
+    '/postBlog.js'
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    if (event.request.url.includes('/img/')) {
+        // Caches image requests dynamically
+        event.respondWith(
+            caches.match(event.request).then(function(response) {
+                return response || fetch(event.request).then(function(fetchResponse) {
+                    return caches.open(CACHE_NAME).then(function(cache) {
+                        cache.put(event.request, fetchResponse.clone());
+                        return fetchResponse;
+                    });
+                });
+            })
+        );
+    } else {
+        // Default caching strategy for other requests
+        event.respondWith(
+            caches.match(event.request).then(function(response) {
+                return response || fetch(event.request);
+            })
+        );
+    }
+});
+
+self.addEventListener('activate', function(event) {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
